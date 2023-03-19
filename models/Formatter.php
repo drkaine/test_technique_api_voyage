985463dd0a25d04b031bb07e083471f6947aea1e
darkaine <duretkevin@live.fr>
2023-03-19 10:49:08 +0100

    Ajout d'une condition pour que le test passe au vert Vérifiction qu'on est un tableau associatif pour le passage en json

--- models/Formatter.php
+++ models/Formatter.php
@@ -11,8 +11,19 @@ class Formatter
 		return json_decode($json, true);
 	}
 
-	public static function transformArrayToJson(array $array): string
+	public static function transformArrayToJson(array $array): string|false
 	{
+		if (!self::isGoodArray($array)) {
+			return false;
+		}
+
 		return json_encode($array);
+	}
+
+	public static function isGoodArray(array $array): bool
+	{
+		return array_reduce(array_keys($array), function ($result, $key) {
+			return $result && is_string($key);
+		}, true);
 	}
 }
