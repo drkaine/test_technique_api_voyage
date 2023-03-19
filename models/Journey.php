cc55221608af3ae9060f098acff6e39f254e7814
darkaine <duretkevin@live.fr>
2023-03-19 11:15:35 +0100

    passage du test au vert Création de la méthode de trie des BoardingCards

--- models/Journey.php
+++ models/Journey.php
@@ -9,4 +9,45 @@ class Journey
 	public function __construct(public array $boardingCardListNotSorted)
 	{
 	}
+
+	public array $boardingCardListSorted;
+
+	public function sortBoardingCardList(): void
+	{
+		$boardingCardList = $this->transformBoardingCardObjectOnArray();
+
+		$boardingCardListFrom = array_column($boardingCardList, 'from');
+		$boardingCardListTo = array_column($boardingCardList, 'to');
+
+		$startJourney = array_values(array_diff($boardingCardListFrom, $boardingCardListTo))[0];
+		$endJourney = array_values(array_diff($boardingCardListTo, $boardingCardListFrom))[0];
+
+		$boardingCardListFrom = array_combine($boardingCardListFrom, $boardingCardList);
+
+		$this->boardingCardListSorted[] = $boardingCardListFrom[$startJourney];
+
+		while ($boardingCardListFrom[$startJourney]['to'] !== $endJourney) {
+			$this->boardingCardListSorted[] = $boardingCardListFrom[$boardingCardListFrom[$startJourney]['to']];
+			$startJourney = $boardingCardListFrom[$startJourney]['to'];
+		}
+	}
+
+	public function transformBoardingCardObjectOnArray(): array
+	{
+		$boardingCardList = [];
+
+		foreach ($this->boardingCardListNotSorted as $boardingCard) {
+			$boardingCardList[] = [
+				'vehicule'        => $boardingCard->vehicule,
+				'from'            => $boardingCard->from,
+				'to'              => $boardingCard->to,
+				'seat'            => $boardingCard->seat,
+				'gate'            => $boardingCard->gate,
+				'baggage'         => $boardingCard->baggage,
+				'vehicule number' => $boardingCard->vehicule_number,
+			];
+		}
+
+		return $boardingCardList;
+	}
 }
