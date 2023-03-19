188c678c06bca2202d59ddff697a13b0d858734e
darkaine <duretkevin@live.fr>
2023-03-19 11:12:22 +0100

    Création d'un test phase rouge Ajout su test de la méthode de trie des cards

--- tests/JourneyTest.php
+++ tests/JourneyTest.php
@@ -24,4 +24,107 @@ class JourneyTest extends TestCase
 
 		$this->assertInstanceOf(BoardingCard::class, $journey->boardingCardListNotSorted[0]);
 	}
+
+	public function testSortBoardingCardList(): void
+	{
+		$journeyListNotSorted = [
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Stockholm',
+				'to'              => 'New York JFK',
+				'seat'            => '7B',
+				'gate'            => '22',
+				'baggage'         => 'auto',
+				'vehicule number' => 'SK22',
+			],
+			[
+				'vehicule'        => 'train',
+				'from'            => 'Madrid',
+				'to'              => 'Barcelona',
+				'seat'            => '45B',
+				'vehicule number' => '78A',
+			],
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Gerona Airport',
+				'to'              => 'Stockholm',
+				'seat'            => '3A',
+				'gate'            => '45B',
+				'baggage'         => 'ticket counter 344',
+				'vehicule number' => 'SK455',
+			],
+			[
+				'vehicule'        => 'bus',
+				'from'            => 'Barcelona',
+				'to'              => 'Gerona Airport',
+				'vehicule number' => null,
+			],
+		];
+
+		$journeyListSorted = [
+			[
+				'vehicule'        => 'train',
+				'from'            => 'Madrid',
+				'to'              => 'Barcelona',
+				'seat'            => '45B',
+				'gate'            => null,
+				'baggage'         => null,
+				'vehicule number' => '78A',
+			],
+			[
+				'vehicule'        => 'bus',
+				'from'            => 'Barcelona',
+				'to'              => 'Gerona Airport',
+				'seat'            => null,
+				'gate'            => null,
+				'baggage'         => null,
+				'vehicule number' => null,
+			],
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Gerona Airport',
+				'to'              => 'Stockholm',
+				'seat'            => '3A',
+				'gate'            => '45B',
+				'baggage'         => 'ticket counter 344',
+				'vehicule number' => 'SK455',
+			],
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Stockholm',
+				'to'              => 'New York JFK',
+				'seat'            => '7B',
+				'gate'            => '22',
+				'baggage'         => 'auto',
+				'vehicule number' => 'SK22',
+			],
+		];
+
+		$journey = new Journey($this->createMockBoardingCardList($journeyListNotSorted));
+
+		$journey->sortBoardingCardList();
+
+		$this->assertEquals($journeyListSorted, $journey->boardingCardListSorted);
+	}
+
+	public function createMockBoardingCardList($journeyListMock): array
+	{
+		$listMock = [];
+
+		foreach ($journeyListMock as $boardingCard) {
+			$boardingCardMock = $this->createMock(BoardingCard::class);
+
+			$boardingCardMock->vehicule = $boardingCard['vehicule'];
+			$boardingCardMock->from = $boardingCard['from'];
+			$boardingCardMock->to = $boardingCard['to'];
+			$boardingCardMock->seat = $boardingCard['seat'];
+			$boardingCardMock->gate = $boardingCard['gate'];
+			$boardingCardMock->baggage = $boardingCard['baggage'];
+            $boardingCardMock->vehicule_number = $boardingCard['vehicule number'];
+
+			$listMock[] = $boardingCardMock;
+		}
+
+		return $listMock;
+	}
 }
