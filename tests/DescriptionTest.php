20a253246a4acb9e83b1d81404cc0318234d6ea3
darkaine <duretkevin@live.fr>
2023-03-19 11:31:04 +0100

    test phase rouge Création d'un test sur la création de la description du voyage

--- tests/DescriptionTest.php
+++ tests/DescriptionTest.php
@@ -56,4 +56,58 @@ class DescriptionTest extends TestCase
 
 		$this->assertInstanceOf(Description::class, $description);
 	}
+
+	public function testGetDescription(): void
+	{
+		$journeyListSorted = [
+			[
+				'vehicule'        => 'train',
+				'from'            => 'Madrid',
+				'to'              => 'Barcelona',
+				'seat'            => '45B',
+				'gate'            => null,
+				'baggage'         => null,
+				'vehicule number' => '78A',
+			],
+			[
+				'vehicule'        => 'bus',
+				'from'            => 'Barcelona',
+				'to'              => 'Gerona Airport',
+				'seat'            => null,
+				'gate'            => null,
+				'baggage'         => null,
+				'vehicule number' => null,
+			],
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Gerona Airport',
+				'to'              => 'Stockholm',
+				'seat'            => '3A',
+				'gate'            => '45B',
+				'baggage'         => 'ticket counter 344',
+				'vehicule number' => 'SK455',
+			],
+			[
+				'vehicule'        => 'flight',
+				'from'            => 'Stockholm',
+				'to'              => 'New York JFK',
+				'seat'            => '7B',
+				'gate'            => '22',
+				'baggage'         => 'auto',
+				'vehicule number' => 'SK22',
+			],
+		];
+
+		$descriptionWanted = [
+			'Take train 78A from Madrid to Barcelona. Sit in seat 45B.',
+			'Take the airport bus from Barcelona to Gerona Airport. No seat assignment.',
+			'From Gerona Airport, take flight SK455 to Stockholm. Gate 45B, seat 3A. Baggage drop at ticket counter 344.',
+			'From Stockholm, take flight SK22 to New York JFK. Gate 22, seat 7B. Baggage will we automatically transferred from your last leg.',
+			'You have arrived at your final destination.',
+		];
+
+		$description = new Description($journeyListSorted);
+
+		$this->assertEquals($descriptionWanted, $description->GetDescription());
+	}
 }
